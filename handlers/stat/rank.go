1a4ebf7f40d542eaf163e39504c0e6a0b114003d
scarletborder <baishuibeef@gmail.com>
2024-06-04 01:11:10 +0800

    fix: fit rank for new index

--- handlers/stat/rank.go
+++ handlers/stat/rank.go
@@ -24,18 +24,30 @@ type result struct {
 	Count    int64  `json:"number"`
 }
 
+type id_result struct {
+	UserID uint64
+	Count  int64
+}
+
 var (
 	rank_result  []result
 	update_stamp int64
 )
 
 func statRank() {
+	var tmp_id_result []id_result
 	global.Init_count.Wait()
 	Db := publish.Reader_DB
 	Db.AutoMigrate(&models.MetaData{})
 
-	Db.Model(&models.MetaData{}).Select("username, count(*) as count").
-		Group("username").Order("count desc").Limit(6).Find(&rank_result)
+	Db.Model(&models.MetaData{}).Select("user_id, count(*) as count").
+		Group("user_id").Order("count desc").Limit(6).Find(&tmp_id_result)
 
+	rank_result = rank_result[:0]
+	for _, val := range tmp_id_result {
+		var tmp_user models.User
+		Db.First(&tmp_user, val.UserID)
+		rank_result = append(rank_result, result{Count: val.Count, Username: tmp_user.Username})
+	}
 	update_stamp = time.Now().UnixNano() / int64(time.Millisecond)
 }
